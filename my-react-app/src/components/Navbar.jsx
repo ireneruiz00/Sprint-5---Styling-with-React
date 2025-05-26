1c6c330843a507b5f978e775682341c7adcd5f67
ireneruiz00 <ruizsirene2@gmail.com>
2025-05-26 22:56:45 +0200

    general structure and configuration

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        esto sera un navabar y tendra cositas chulas
+        <h1>esto sera un titulo</h1>
+        <p>este sera un parrafo</p>
+        
+    </div>
+  )
+}
+
+export default Navbar
